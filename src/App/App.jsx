0564b204bf29b1071416e45a99f511d2b7fdcc1d
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2023-04-29 19:33:16 -0300

    initial commit

---
+++ src/App/App.jsx
@@ -0,0 +1,12 @@
+import './App.css'
+import AnotherComponent from '../Components/AnotherComponent/AnotherComponent'
+
+function App() {
+  return (
+    
+    <AnotherComponent booleanoExemplo={true} />
+
+  )
+}
+
+export default App
