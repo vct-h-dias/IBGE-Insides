cfdefc4a98496c7b1e5869e678ced6ff9eae5bf2
Camargo <matheus.ginebro@aluno.ifsp.edu.br>
2023-05-28 11:41:08 -0300

    landing page 2/3 -> body done.

--- src/Components/Landing-Page-Components/Body/Body-Components/card/Card.jsx
+++ src/Components/Landing-Page-Components/Body/Body-Components/card/Card.jsx
@@ -1,12 +1,29 @@
 import '../card/Card.css'
 
-const Card = ({ title, text, image}) => {
-    return (
+const Card = ({ title, text, image, direction}) => {
+    return(
+        direction == "left" ?
         <>
-            <div className="card-container-div">
-                <h1>{title}</h1>
-                <p>{text}</p>
-                <img src="../../../" alt="representation" />
+            <div className="card-container-div left">
+                <div className="image-div">
+                    <img src={image} alt="representation" />
+                </div>
+                <div className="info-div">
+                    <h1>{title}</h1>
+                    <p>{text}</p>
+                </div>
+            </div> 
+        </>
+        :
+        <>
+            <div className="card-container-div right">
+                <div className="info-div">
+                    <h1>{title}</h1>
+                    <p>{text}</p>
+                </div>
+                <div className="image-div">
+                    <img src={image} alt="representation" />
+                </div>
             </div> 
         </>
     )
