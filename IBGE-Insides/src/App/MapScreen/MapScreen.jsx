48b2fbd633a0b5f4191109734bd918edb87a4941
Camargo <110298379+MatheusCamargoGinebro@users.noreply.github.com>
2023-06-11 14:47:06 -0300

    finalização inicial do body. (#18)
    O que foi feito:
    - Finalização do carrousel;
    - Add Cards de desenvolvedores (mudar foto do vitão);
    - Add Parte de instituições;
    
    O que falta fazer:
    - Adcionar o Footer;
    - Terminar a tela do mapa;

---
+++ IBGE-Insides/src/App/MapScreen/MapScreen.jsx
@@ -0,0 +1,16 @@
+import './MapScreen.css'
+import NavCorner from '../../Components/Map-Components/NavCorner/NavCorner.jsx'
+import MapBody from '../../Components/Map-Components/MapBody/MapBody'
+
+function MapScreen() {
+  return (
+
+    <>
+      <MapBody />
+      {/* <NavCorner isMap={true} /> */}
+    </>
+
+  )
+}
+
+export default MapScreen
