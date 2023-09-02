6a8386b9770d209582bc63c2b401d0a029922e83
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2023-09-02 00:51:17 -0300

    scroll limits rules

--- src/Components/Map-Components/MapBody/MapBody.jsx
+++ src/Components/Map-Components/MapBody/MapBody.jsx
@@ -1,21 +1,42 @@
-import Map from 'react-map-gl';
-import './MapBody.css'
+import Map from "react-map-gl";
+import "./MapBody.css";
+import * as turf from "@turf/turf";
+import { useCallback, useState } from "react";
+
+const GEOFENCE = turf.circle([-52.4, -16.3], 5400, { units: "kilometers" });
 
 function MapBody() {
-  return (
+  const [viewState, setViewState] = useState({
+    longitude: -52.4,
+    latitude: -16.3,
+    zoom: 3.8,
+  });
 
+  const onMove = useCallback(({ viewState }) => {
+    const newCenter = [viewState.longitude, viewState.latitude, viewState.zoom];
+
+    if (turf.booleanPointInPolygon(newCenter, GEOFENCE)) {
+      setViewState({
+        longitude: viewState.longitude,
+        latitude: viewState.latitude,
+        zoom: viewState.zoom,
+      });
+    }
+    console.log(viewState, "currentView");
+  }, []);
+
+  return (
     <Map
+      {...viewState}
+      onMove={onMove}
       mapboxAccessToken="pk.eyJ1IjoiY2FtYXJnMHZzIiwiYSI6ImNsbTFiczBubzBlZWEzanBla29sMHFhNngifQ.n91ThdPn4IujuqZy8V4mOg"
-      initialViewState={{
-        longitude: -52.4,
-        latitude: -16.3,
-        zoom: 3.8
-      }}
-      scrollZoom={false}
-      style={{width: '100%', height: '100vh'}}
+      maxZoom={5.6}
+      minZoom={3.9}
+      doubleClickZoom={false}
+      style={{ width: "100%", height: "100vh" }}
       mapStyle="mapbox://styles/camarg0vs/clm1c13c401ub01p7g8sngg8x"
     />
   );
 }
 
-export default MapBody
+export default MapBody;
