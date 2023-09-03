395ef913b59faee43622d1420a753112a6f1ba61
vct-h-dias <d.hugo@aluno.ifsp.edu.br>
2023-09-03 01:17:29 -0300

    https fetch to vercel prod

--- src/Components/Map-Components/MapBody/getState.js
+++ src/Components/Map-Components/MapBody/getState.js
@@ -5,7 +5,7 @@ export default async function getStates() {
       const response = [];
       states.forEach(async (state) => {
         const req = await fetch(
-          `http://servicodados.ibge.gov.br/api/v3/malhas/estados/${state.sigla}?formato=application%2Fvnd.geo%2Bjson`
+          `https://servicodados.ibge.gov.br/api/v3/malhas/estados/${state.sigla}?formato=application%2Fvnd.geo%2Bjson`
         );
         const json = await req.json();
         json["sigla"] = state.sigla;
