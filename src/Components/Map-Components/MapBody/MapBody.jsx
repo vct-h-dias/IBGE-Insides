b8fc9909e804863a7018e50760a6ba10778c2d43
Victor Hugo <d.hugo@aluno.ifsp.edu.br>
2023-09-11 14:19:36 -0300

    hover attempt

--- src/Components/Map-Components/MapBody/MapBody.jsx
+++ src/Components/Map-Components/MapBody/MapBody.jsx
@@ -1,44 +1,37 @@
 import Map, { Source, Layer } from "react-map-gl";
 import * as turf from "@turf/turf";
-import { useCallback, useState } from "react";
+import { useCallback, useMemo, useState } from "react";
 // import getStates from "./getState"; //hardcode :(
-import geojson from "./geojson.json"; 
+import geojson from "./geojson.json";
 
 function MapBody() {
   const [hoverInfo, setHoverInfo] = useState(null);
-  
+
   const onHover = useCallback((event) => {
-    const {
-      features,
-      point: { x, y },
-    } = event;
-    const hoveredFeature = features && features[0];
-    
-    setHoverInfo(hoveredFeature && { feature: hoveredFeature, x, y });
+    // console.log(event, 'event')
+    const county = event.features[0] ? event.features[0].properties.estado : ''
+    setHoverInfo({
+      countyName: county,
+      longitude: event.lngLat.lng,
+      latitude: event.lngLat.lat,
+    });
   }, []);
-  
+
   const [viewState, setViewState] = useState({
     longitude: -47,
     latitude: -15,
     zoom: 3.8,
   });
-  
-  // Definindo os limites que o usuário pode navegar pelo mapa:
-  const GEOFENCE = turf.circle([-52.4, -16.3], 5400, { units: "kilometers" });
-  
+
   // Definindo a função que limita o usuário a navegar apenas dentro do GEOFENCE:
   const onMove = useCallback(({ viewState }) => {
-    const newCenter = [viewState.longitude, viewState.latitude, viewState.zoom];
-    
-    if (turf.booleanPointInPolygon(newCenter, GEOFENCE)) {
-      setViewState({
-        longitude: viewState.longitude,
-        latitude: viewState.latitude,
-        zoom: viewState.zoom,
-      });
-    }
+    setViewState({
+      longitude: viewState.longitude,
+      latitude: viewState.latitude,
+      zoom: viewState.zoom,
+    });
   }, []);
-  
+
   const layerStyle = {
     id: "data",
     type: "fill",
@@ -48,43 +41,61 @@ function MapBody() {
     },
   };
 
-  // Retornando o componente MapBody:
-  return (
-    /* OBS: Eu sei que era melhor só retornar o componente map... mas assim a gente consegue tirar o scrollBar */
-    <div className="overflow-hidden w-full h-full">
-       <Map
+  const layerHighlight = {
+    id: "data",
+    type: "fill",
+    paint: {
+      'fill-outline-color': '#484896',
+      'fill-color': '#6e599f',
+      'fill-opacity': 0.75
+    } 
+};
+
+// console.log(hoverInfo)
+
+const selectedCounty = (hoverInfo && hoverInfo.countyName) || '';
+console.log(selectedCounty)
+const filter = useMemo(
+  () => ['in', 'COUNTY', selectedCounty],
+  [selectedCounty]
+);  
+// Retornando o componente MapBody:
+return (
+  /* OBS: Eu sei que era melhor só retornar o componente map... mas assim a gente consegue tirar o scrollBar */
+  <div className="overflow-hidden w-full h-full bg-slate-400">
+    <Map
       {...viewState}
       onMove={onMove}
-      mapboxAccessToken="pk.eyJ1IjoiY2FtYXJnMHZzIiwiYSI6ImNsbTFiczBubzBlZWEzanBla29sMHFhNngifQ.n91ThdPn4IujuqZy8V4mOg"
+      mapboxAccessToken="pk.eyJ1IjoiY2FtYXJnMHZzIiwiYSI6ImNsbGtyeHkwNzIzYXYzcW8xYTk4dXplOTkifQ.AeclKAsX4UhZf6xCfZgwPg"
       maxZoom={5.6}
-      minZoom={3.0}
+      minZoom={2.0}
       interactiveLayerIds={["data"]}
       doubleClickZoom={false}
       style={{ width: "100%", height: "100vh" }}
       mapStyle="mapbox://styles/camarg0vs/clm1c13c401ub01p7g8sngg8x"
       onMouseMove={onHover}
       onClick={() => {
-      console.log(hoverInfo);
+        console.log(hoverInfo);
       }}
     >
       {/* Colocando a camada dos estados */}
       <Source type="geojson" data={geojson}>
-        <Layer {...layerStyle}  />
+        <Layer {...layerStyle} />
+        <Layer {...layerStyle} filter={filter} /> 
       </Source>
 
       {/* paint={!!hoverInfo && ({"fill-color": 0 ? "#000000" : "#ffffff",})} */}
 
       {/* div de informações */}
-      {!!hoverInfo && (
-        <div className="tooltip absolute z-10 p-2 bg-blue-200 pointer-events-none top-0 right-0 w-56 text-right">
-          <div>Estado: {hoverInfo.feature.properties.estado}</div>
-          <div>Sigla: {hoverInfo.feature.properties.sigla}</div>
-          <div>Code Area: {hoverInfo.feature.properties.codarea}</div>
+      {/* {!!hoverInfo.countyName && (
+        <div className="tooltip absolute z-10 p-2 bg-slate-800 text-slate-50 m-2 pointer-events-none top-0 right-0 w-56 text-right">
+          <div>Estado: {hoverInfo.countyName}</div>
         </div>
-      )}
+      )}  */}
     </Map>
-    </div>
-  );
+  </div>
+);
 }
+
 
 export default MapBody;
--- src/Components/Map-Components/NavCorner/NavCorner.jsx
+++ src/Components/Map-Components/NavCorner/NavCorner.jsx
@@ -4,12 +4,14 @@ import {BsBoxArrowInLeft} from 'react-icons/bs'
 const NavCorner = () => {
 
     return (
-        <>
-            <div className='fixed z-10 w-52 h-5 bg-slate-800'>
-                <button>
-                    <BsBoxArrowInLeft className='text-slate-50' size={35}/>
-                </button>
-            </div>  
+        <>  
+            <div className='fixed m-2 top-0 h-fit w-fit flex justify-center items-center gap-1'>
+                <a href='/'>
+                    <div className='fixed flex items-center gap-1 z-10 w-fit pl-3 h-fit p-2 bg-slate-800 text-slate-50 hover:bg-slate-700 duration-75'>
+                        <BsBoxArrowInLeft className='' size={35}/> Voltar para Home
+                    </div>
+                </a>
+            </div>
         </>
     )
 }
